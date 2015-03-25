ba9e2afa41a37d986b25acd6e6eed55e946a8ff5
Pedro Valentim <pedroavalentim@gmail.com>
2015-03-25 00:23:43 +0000

    Fix tests

--- test/test.js
+++ test/test.js
@@ -52,7 +52,7 @@ describe('cli', function () {
   });
 
   it('should read from a file and write to a file', function (done) {
-    exec('bin/sourdough test/fixtures/cli/input.wcss test/fixtures/cli/output.css', function (err, stdout) {
+    exec('bin/sourdough test/fixtures/cli/input.styl test/fixtures/cli/output.css', function (err, stdout) {
       if (err) return done(err);
       var res = read('fixtures/cli/output', '.css');
       assert.equal(res, output);
@@ -61,7 +61,7 @@ describe('cli', function () {
   });
 
   it('should read from a file and write to stdout', function (done) {
-    exec('bin/sourdough test/fixtures/cli/input.wcss', function (err, stdout) {
+    exec('bin/sourdough test/fixtures/cli/input.styl', function (err, stdout) {
       if (err) return done(err);
       assert.equal(stdout, output);
       done();
@@ -80,7 +80,7 @@ describe('cli', function () {
   });
 
   it('should log on verbose', function (done) {
-    exec('bin/sourdough -v test/fixtures/cli/input.wcss test/fixtures/cli/output.css', function (err, stdout) {
+    exec('bin/sourdough -v test/fixtures/cli/input.styl test/fixtures/cli/output.css', function (err, stdout) {
       if (err) return done(err);
       assert(-1 != stdout.indexOf('write'));
       done();
@@ -88,7 +88,7 @@ describe('cli', function () {
   });
 
   it('should allow configurable import root', function (done) {
-    exec('bin/sourdough -i test/fixtures test/fixtures/import.wcss test/fixtures/cli/output.css', function (err, stdout) {
+    exec('bin/sourdough -i test/fixtures test/fixtures/import.styl test/fixtures/cli/output.css', function (err, stdout) {
       if (err) return done(err);
       var res = read('fixtures/cli/output', '.css');
       var expected = read('fixtures/import.out', '.css');
@@ -98,7 +98,7 @@ describe('cli', function () {
   });
 
   it('should log on non-existant file', function (done) {
-    exec('bin/sourdough test/fixtures/cli/non-existant.wcss', function (err, stdout, stderr) {
+    exec('bin/sourdough test/fixtures/cli/non-existant.styl', function (err, stdout, stderr) {
       assert(err);
       assert(err.code == 1);
       assert(-1 != stderr.indexOf('not found'));
@@ -107,7 +107,7 @@ describe('cli', function () {
   });
 
   xit('should print a nice error', function (done) {
-    exec('bin/sourdough test/fixtures/cli/error.wcss', function (err, stdout, stderr) {
+    exec('bin/sourdough test/fixtures/cli/error.styl', function (err, stdout, stderr) {
       assert(err);
       assert(err.code == 1);
       assert(-1 != stderr.indexOf('error'));
@@ -152,6 +152,6 @@ function remove (filename) {
  */
 
 function resolve (filename, ext) {
-  if (ext === undefined) ext = '.wcss'
+  if (ext === undefined) ext = '.styl'
   return path.resolve(__dirname, filename + ext);
 }
